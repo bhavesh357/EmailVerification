665b3e7106fc2287d566b65c7805f5290acc5578
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-11 14:28:16 +0530

    ADDED: function to validate email

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,1 +1,17 @@
 echo Welcome to email verification
+pat=""
+
+function validate() {
+	email=$1
+	pat="[A-Za-z\d]{1,}"
+	if [[ $email =~ $pat ]]
+	then
+		echo good email
+	else
+		echo enter proper email
+	fi
+}
+
+echo "Enter email id"
+read email
+validate $email
